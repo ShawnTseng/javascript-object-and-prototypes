b479828d1f35c1c4577cdaeb7c7e4323404c1a30
ShawnTseng <shawntseng40@gmail.com>
2020-02-24 09:00:55 +0800

    add: common logic about display

---
+++ display.js
@@ -0,0 +1,30 @@
+function display() {
+  for (let i = 0; i < arguments.length; i++) {
+    if (typeof arguments[i] === "object") {
+      displayObject(arguments[i]);
+    } else {
+      displayValue(arguments[i], true);
+    }
+  }
+}
+
+function displayObject(object) {
+  displayValue(getTypeName(object) + " {");
+  for (const propertyName in object) {
+    displayValue(propertyName + ": " + object[propertyName]);
+  }
+  displayValue("}", true);
+}
+
+function displayValue(value, addMargin, addPadding) {
+  const div = document.createElement("div");
+  div.style.fontSize = "32px";
+  if (addMargin) {
+    div.style.marginBottom = "30px";
+  }
+  if (addPadding) {
+    div.style.paddingLeft = "30px";
+  }
+  div.innerText = value;
+  document.body.appendChild(div);;
+}
