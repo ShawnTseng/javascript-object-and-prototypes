9b542665758695d0882a12b430f253348d19e37a
ShawnTseng <shawntseng40@gmail.com>
2020-02-24 10:13:46 +0800

    using Object.create()

--- script.js
+++ script.js
@@ -14,11 +14,26 @@
 // display(cat.age);
 // cat.speak();
 
-function Cat(name, color) {
-  this.name = name;
-  this.color = color;
-}
+// function Cat(name, color) {
+//   this.name = name;
+//   this.color = color;
+// }
 
-var cat = new Cat("Fluffy", "White");
+// var cat = new Cat("Fluffy", "White");
+
+var cat = Object.create(Object.prototype, {
+  name: {
+    value: "Fluffy",
+    enumerable: true,
+    writable: true,
+    configurable: true
+  },
+  color: {
+    value: "White",
+    enumerable: true,
+    writable: true,
+    configurable: true
+  }
+});
 
 display(cat);
