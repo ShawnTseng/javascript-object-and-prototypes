8679040c8af47e2f6fb1eeacca2f3b39bb73e202
ShawnTseng <shawntseng40@gmail.com>
2020-02-24 10:20:15 +0800

    using syntax: ES6 Class

--- script.js
+++ script.js
@@ -21,19 +21,32 @@
 
 // var cat = new Cat("Fluffy", "White");
 
-var cat = Object.create(Object.prototype, {
-  name: {
-    value: "Fluffy",
-    enumerable: true,
-    writable: true,
-    configurable: true
-  },
-  color: {
-    value: "White",
-    enumerable: true,
-    writable: true,
-    configurable: true
+// var cat = Object.create(Object.prototype, {
+//   name: {
+//     value: "Fluffy",
+//     enumerable: true,
+//     writable: true,
+//     configurable: true
+//   },
+//   color: {
+//     value: "White",
+//     enumerable: true,
+//     writable: true,
+//     configurable: true
+//   }
+// });
+
+class Cat {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
   }
-});
+
+  speak() {
+    display("Meeoooooooow");
+  }
+}
+
+var cat = new Cat("Fluffy", "White");
 
 display(cat);
