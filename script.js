098122399d6f3882485140047a20dd37425a4218
ShawnTseng <shawntseng40@gmail.com>
2020-02-26 14:28:42 +0800

    play function's prototype

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+function Cat(name, color) {
+  this.name = name;
+  this.color = color;
+}
+
+Cat.prototype.age = 4;
+
+var fluffy = new Cat("Fluffy", "Grey");
+var muffin = new Cat("Muffin", "Brown");
+
+Cat.prototype = { age: 5 };
+
+var showbell = new Cat("Snowbell", "White");
+
+display(fluffy.age);
+display(muffin.age);
+display(showbell.age);
+display(Cat.prototype.age);
